a3e7470d5e018364895330ceadc8ea99e801d2a2
ceastld <1950946176@qq.com>
2025-01-27 22:11:46 +0800

    setup

--- create_env.sh
+++ create_env.sh
@@ -3,6 +3,7 @@ ml CUDA/10.1.243
 conda create -n infoswap python=3.6 -y
 conda activate infoswap
 pip install -r requirements.txt
-pip install opencv-python
+pip install opencv-python==4.4.0.46
+pip install -e ./preprocess
 pip install gdown
 gdown --folder https://drive.google.com/drive/folders/19d1T4Sr59iMMHi7MU5eFmmRX3U0nbfPa
